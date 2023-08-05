cd42ebb620033e5db64bacbbbb4c01ba65c17608
Esteban Donis <estebandonis10@gmail.com>
2023-08-04 18:29:57 -0600

    Casi terminado

---
+++ README.md
@@ -0,0 +1,15 @@
+# CC2018-lab2
+Utilizar SDL para simular el algoritmo de The Game of Life de Conway
+## Using MacOS
+Para ejecutarlo, debemos de tener los frameworks de SDL en el folder de /Library/Frameworks
+y debemos de asegurarnos tener la librería de glm, cambiar dirección en CMakeLists.txt
+
+Y luego ejecutar el siguiente comando en la terminal:
+```shell
+./run.sh
+```
+
+Al ejecutarlo se verá algo así:
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <glm.hpp>
 #include <SDL2/SDL.h>
+#include <vector>
+#include <string>
 
 // Define the size of the framebuffer
 const int FRAMEBUFFER_WIDTH = 500;
@@ -26,8 +30,14 @@ Color clearColor = {0, 0, 0, 255}; // Initially set to black
 // Declare a global currentColor of type Color
 Color currentColor = {255, 255, 255, 255}; // Initially set to white
 
-int x = 5;
-int y = 5;
+int x = 50;
+int y = 50;
+
+void clear() {
+    for (int i = 0; i < FRAMEBUFFER_SIZE; i++) {
+        framebuffer[i] = clearColor;
+    }
+}
 
 // Function to set a specific pixel in the framebuffer to the currentColor
 void point(int x, int y) {
@@ -98,9 +108,73 @@ void triangle(const glm::vec3& A, const glm::vec3& B, const glm::vec3& C) {
     line(C, A);
 }
 
-void clear() {
-    for (int i = 0; i < FRAMEBUFFER_SIZE; i++) {
-        framebuffer[i] = clearColor;
+struct Face {
+    std::vector<std::array<int, 3>> vertexIndices;
+};
+
+bool loadOBJ(const std::string& path, std::vector<glm::vec3>& out_vertices, std::vector<Face>& out_faces, float scale) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << path << std::endl;
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string token;
+        iss >> token;
+
+        if (token == "v") {
+            glm::vec3 vertex;
+            iss >> vertex.x >> vertex.y >> vertex.z;
+
+            // Apply scaling to the vertex coordinates
+            vertex *= scale;
+
+            out_vertices.push_back(vertex);
+        } else if (token == "f") {
+            Face face;
+            int v1, v2, v3;
+            char slash;
+            while (iss >> v1 >> slash >> v2 >> slash >> v3) {
+                face.vertexIndices.push_back({v1 - 1, v2 - 1, v3 - 1}); // OBJ indices are 1-based
+            }
+            out_faces.push_back(face);
+        }
+    }
+
+    file.close();
+    return true;
+}
+
+std::vector<glm::vec3> setupVertexArray(const std::vector<glm::vec3>& vertices, const std::vector<Face>& faces) {
+    std::vector<glm::vec3> vertexArray;
+
+    // For each face
+    for (const auto& face : faces) {
+        // For each vertex in the face
+        for (const auto& vertexIndices : face.vertexIndices) {
+            // Get the vertex position from the input array using the index from the face
+            glm::vec3 vertexPosition = vertices[vertexIndices[0]];
+
+            // Add the vertex position to the vertex array
+            vertexArray.push_back(vertexPosition);
+        }
+    }
+
+    return vertexArray;
+}
+
+void drawObject(const std::vector<glm::vec3>& combinedVertexArray, const std::vector<Face>& faces) {
+    for (const auto& face : faces) {
+        for (const auto& vertexIndices : face.vertexIndices) {
+            glm::vec3 vertexA = combinedVertexArray[vertexIndices[0]];
+            glm::vec3 vertexB = combinedVertexArray[vertexIndices[1]];
+            glm::vec3 vertexC = combinedVertexArray[vertexIndices[2]];
+
+            triangle(vertexA, vertexB, vertexC);
+        }
     }
 }
 
@@ -109,14 +183,32 @@ int main() {
 
     SDL_Window* window = SDL_CreateWindow("SR", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(
-        window,
-        -1,
-        SDL_RENDERER_ACCELERATED
-    );
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     bool running = true;
     SDL_Event event;
+
+    std::vector<glm::vec3> vertices;
+    std::vector<Face> faces;
+
+    if (loadOBJ("/Users/estebandonis/Documents/SextoSemestre/GraficasPorComputadora/Ejemplos/Blender/cube.obj", vertices, faces, 80.0f)) {
+        // Successfully loaded OBJ file
+        // Now you can use the vertices and faces vectors
+        // ...
+        std::cout << "Leido" << std::endl;
+
+        std::vector<glm::vec3> combinedVertexArray = setupVertexArray(vertices, faces);
+
+        glm::vec3 translationOffset(250, 250, 0.0f); // Adjust x and y as needed
+
+        // Apply the translation to each vertex in the combinedVertexArray
+        for (auto& vertex : combinedVertexArray) {
+            vertex += translationOffset;
+        }
+
+        drawObject(combinedVertexArray, faces);
+    }
+
 
     while (running) {
         while (SDL_PollEvent(&event)) {
@@ -124,15 +216,6 @@ int main() {
                 running = false;
             }
         }
-
-        clear();
-
-        glm::vec3 A(100, 100, 0); // Vértice A del triángulo
-        glm::vec3 B(300, 500, 0); // Vértice B del triángulo
-        glm::vec3 C(500, 200, 0); // Vértice C del triángulo
-
-        triangle(A, B, C);
-
 
         // Call our render function
         renderBuffer(renderer);
